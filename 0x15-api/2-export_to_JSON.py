763a1b813402be8feee68925fced157071238851
Danny Hollman <dannyhollman@gmail.com>
2020-02-03 23:29:52 +0000

    [INIT] Add file 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" returns TODO list from API """
+import csv
+import json
+import requests
+from sys import argv
+
+
+def main():
+    """ returns TODO list """
+    total = 0
+    complete = 0
+    user_url = "https://jsonplaceholder.typicode.com/users/"
+    task_url = "https://jsonplaceholder.typicode.com/todos?userId="
+    user = requests.get('{}{}'.format(user_url, argv[1])).json()
+    tasks = requests.get('{}{}'.format(task_url, argv[1])).json()
+
+    for task in tasks:
+        task['username'] = user['username']
+        del task['id']
+
+    with open("{}.json".format(argv[1]), "w") as f:
+        json.dump({argv[1]: tasks}, f)
+
+if __name__ == "__main__":
+    main()
