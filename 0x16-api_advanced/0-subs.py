636f4eaf85e7f5645374a4bc43766a64ddae4e77
Danny Hollman <dannyhollman@gmail.com>
2020-02-06 22:51:42 +0000

    [UPDATE] Fix url, remove name == main

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """ returns subscriber counter for subreddit """
 import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
     """ returns subscriber count """
-    url = "http://www.reddit.com/r/{}/about".format(subreddit)
+    url = "https://api.reddit.com/r/{}/about".format(subreddit)
     response = requests.get(url, headers={'User-Agent': 'test'}).json()
     try:
         return response['data']['subscribers']
     except:
         return 0
-
-if __name__ == "__main__":
-    number_of_subscribers()
