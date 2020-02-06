dc91d4778a6f0bf207fe4e13258025432607549e
Danny Hollman <dannyhollman@gmail.com>
2020-02-06 22:23:19 +0000

    [UPDATE] Remove loop, change url

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,16 +4,14 @@ import requests
 from sys import argv
 
 
-def number_of_subscribers(arg):
+def number_of_subscribers(subreddit):
     """ returns subscriber count """
-    url = "http://www.reddit.com/r/{}/about.json".format(arg)
+    url = "http://www.reddit.com/r/{}/about".format(subreddit)
     response = requests.get(url, headers={'User-Agent': 'test'}).json()
-    for k, v in response.items():
-        if k == "data":
-            try:
-                return v['subscribers']
-            except:
-                return 0
+    try:
+        return response['data']['subscribers']
+    except:
+        return 0
 
 if __name__ == "__main__":
     number_of_subscribers()
