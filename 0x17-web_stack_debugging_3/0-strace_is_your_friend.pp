9289d486e4b2c515b33c1b08a5c079daf8a88844
Danny Hollman <dannyhollman@gmail.com>
2020-02-20 01:23:31 +0000

    [UPDATE] Fix sed command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fix typo with puppet
 
-exec { 'fix typo':
-  command => '/bin/sed -i \'s/\.phpp/\.php\' /var/www/html/wp-settings.php'
+exec { 'typo':
+  command => "/bin/sed -i \'s/phpp/php/\' /var/www/html/wp-settings.php"
 }
