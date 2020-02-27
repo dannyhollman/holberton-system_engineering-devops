e7e249ce07c10db8e8e7b32e22f4e5989318ea28
Danny Hollman <dannyhollman@gmail.com>
2020-02-27 23:42:03 +0000

    [UPDATE] Add comment

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,4 @@
+# Remove request limit and restart nginx
 exec { 'removeLimit':
   path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   command => "sudo sed -i 's/15/20000/g' /etc/default/nginx; sudo service nginx restart",
