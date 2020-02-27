beb68c4f48142eb902b2b0f6773d830a6b2e3349
Danny Hollman <dannyhollman@gmail.com>
2020-02-27 22:31:36 +0000

    [UPDATE] Add path for commands

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,4 @@
 exec { 'removeLimit':
-  command => "/bin/sed -i 's/15/4096/g' /etc/default/nginx; sudo /usr/bin/service nginx restart"
+  path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command => "sed -i 's/15/4096/g' /etc/default/nginx; sudo service nginx restart",
 }
