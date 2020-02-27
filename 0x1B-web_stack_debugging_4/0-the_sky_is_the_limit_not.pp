c918d021949e2acc335639923554f3cb4a7fb536
Danny Hollman <dannyhollman@gmail.com>
2020-02-27 23:39:53 +0000

    [UPDATE] Raise limit, add sudo to sed command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
 exec { 'removeLimit':
   path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => "sed -i 's/15/4096/g' /etc/default/nginx; sudo service nginx restart",
+  command => "sudo sed -i 's/15/20000/g' /etc/default/nginx; sudo service nginx restart",
 }
