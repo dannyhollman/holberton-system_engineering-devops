d75e2046deb52fda70c7f83004807f6603310168
Danny Hollman <dannyhollman@gmail.com>
2020-02-27 22:47:25 +0000

    [UPDATE] Fix puppet-lint error

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
 exec { 'removeLimit':
-  path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   command => "sed -i 's/15/4096/g' /etc/default/nginx; sudo service nginx restart",
 }
